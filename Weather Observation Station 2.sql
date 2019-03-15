a8b4f131977b1d63dc2c54ae56a8e1cc5c5dac64
mohand reda sakr <mohandsakr69@gmail.com>
2019-03-15 12:06:07 +0200

    Create Weather Observation Station 2.sql

---
+++ Weather Observation Station 2.sql
@@ -0,0 +1,9 @@
+
+/*
+problem link: https://www.hackerrank.com/challenges/weather-observation-station-2/problem
+status : accepted 
+problem name :Weather Observation Station 2
+
+ 
+*/
+select  round (sum (LAT_N ),2)  ,round (sum (LONG_W  ),2) from STATION ; 
