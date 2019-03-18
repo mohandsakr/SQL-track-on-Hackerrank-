4e8fcbd0e5ab06ca16998bec886ef7b85196a84e
mohand reda sakr <mohandsakr69@gmail.com>
2019-03-18 13:48:21 +0200

    Create Japan Population.sql

---
+++ Japan Population.sql
@@ -0,0 +1,6 @@
+/*
+problem link: https://www.hackerrank.com/challenges/japan-population/problem
+status :accepted
+problem name:Japan Population
+*/
+select  sum(population ) from CITY where COUNTRYCODE ='JPN';
