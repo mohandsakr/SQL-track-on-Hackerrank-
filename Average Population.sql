8a792987c9577dfd72cbe1c44622bbdb73258f4d
mohand reda sakr <mohandsakr69@gmail.com>
2019-03-18 13:45:22 +0200

    Create Average Population.sql

---
+++ Average Population.sql
@@ -0,0 +1,8 @@
+/*
+
+problem link: https://www.hackerrank.com/challenges/average-population/problem
+status :accepted
+problem name:Average Population
+
+*/
+select floor(avg(population )) from CITY;
