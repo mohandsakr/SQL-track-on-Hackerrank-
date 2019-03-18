a51a67a62c37a26c49dc586ab9c3b012edcab1bd
mohand reda sakr <mohandsakr69@gmail.com>
2019-03-18 13:18:36 +0200

    Create Revising Aggregations - The Sum Function.sql

---
+++ Revising Aggregations - The Sum Function.sql
@@ -0,0 +1,8 @@
+/*
+problem name:Revising Aggregations - The Sum Function
+problem link:https://www.hackerrank.com/challenges/revising-aggregations-sum/problem
+status :accepted
+
+
+*/
+select sum(population) from city  where District = 'California';
