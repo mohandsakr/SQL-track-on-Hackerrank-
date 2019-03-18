50913e757cee96400fdeb2a459356df7eee25395
mohand reda sakr <mohandsakr69@gmail.com>
2019-03-18 13:50:45 +0200

    Create Population Density Difference.sql

---
+++ Population Density Difference.sql
@@ -0,0 +1,7 @@
+/*
+problem link:  https://www.hackerrank.com/challenges/population-density-difference/problem
+status :accepted
+problem name:Population Density Difference.sql
+*/
+
+select max(population)-min(population ) from city ;
