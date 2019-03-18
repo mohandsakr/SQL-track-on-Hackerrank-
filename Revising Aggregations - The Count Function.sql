0ddc572c1bd33f60c3a478f064931ebe37d98038
mohand reda sakr <mohandsakr69@gmail.com>
2019-03-18 13:13:31 +0200

    Create Revising Aggregations - The Count Function.sql

---
+++ Revising Aggregations - The Count Function.sql
@@ -0,0 +1,10 @@
+/*
+problem link:https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+status :accepted
+problem name:Revising Aggregations - The Count Function
+
+
+
+
+*/
+select count(name)  from CITY  where Population >100000;
