b65d9f945a75a72b870403a9ca3f4df7a56b8226
mohand reda sakr <mohandsakr69@gmail.com>
2019-03-20 15:39:26 +0200

    Update Weather Observation Station 18.sql

--- Weather Observation Station 18.sql
+++ Weather Observation Station 18.sql
@@ -1,12 +1,34 @@
 
 
 /*
-problem link:  https://www.hackerrank.com/challenges/weather-observation-station-5/problem
+problem link:   https://www.hackerrank.com/challenges/weather-observation-station-18/problem
 status :accepted
-problem name: Weather Observation Station 18*/
+problem name: Weather Observation Station 18 * /
+
+
 /*
-Enter your query here.
-Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+ Consider P1(a,b),P2(c,d) and
+
+to be two points on a 2D plane.
+
+ * a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+ * b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+ * c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+ * d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+
+Query the Manhattan Distance between points
+and and round it to a scale of
+
+decimal places.
+
+Input Format
+
+The STATION table is described as follows:
+
+[Station.jpg]
+
+where LAT_N is the northern latitude and LONG_W is the western longitude. 
+ 
 */
 
 select round((abs(min(LAT_N)-max(LAT_N))+abs(min(LONG_W )-max(LONG_W ))),4) from station ;
