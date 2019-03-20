acb3a2ce24fc593f23ccd0154803ad273b6a2f3c
mohand reda sakr <mohandsakr69@gmail.com>
2019-03-20 15:16:11 +0200

    Create Revising Aggregations - Averages.sql

---
+++ Revising Aggregations - Averages.sql
@@ -0,0 +1,7 @@
+/*
+problem link:  https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem
+status :accepted
+problem name: Revising Aggregations - Averages */
+
+
+select avg(population) from city where District = 'California';
