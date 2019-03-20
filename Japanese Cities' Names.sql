695ab4b08cad3f25e93d9b9eaac9f026ee3a803d
mohand reda sakr <mohandsakr69@gmail.com>
2019-03-20 14:57:56 +0200

    Create Japanese Cities' Names.sql

---
+++ Japanese Cities' Names.sql
@@ -0,0 +1,7 @@
+
+/*
+problem link:  https://www.hackerrank.com/challenges/japanese-cities-name/problem
+status :accepted
+problem name:Average Population
+*/
+select  name from CITY where COUNTRYCODE='JPN';
