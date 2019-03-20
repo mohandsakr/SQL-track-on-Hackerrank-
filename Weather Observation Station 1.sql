d807dd21c721d07af872d3c75d2df065f643b55a
mohand reda sakr <mohandsakr69@gmail.com>
2019-03-20 14:59:16 +0200

    Create Weather Observation Station 1.sql

---
+++ Weather Observation Station 1.sql
@@ -0,0 +1,11 @@
+/*
+problem link:  https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+status :accepted
+problem name: Weather Observation Station 1
+*/
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+select city,state  from STATION ;
