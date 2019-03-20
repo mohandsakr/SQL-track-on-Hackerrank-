b79342947e3df7c6d90f76e35d7b32c28e58255b
mohand reda sakr <mohandsakr69@gmail.com>
2019-03-20 14:50:55 +0200

    Create Revising the Select Query II.sql

---
+++ Revising the Select Query II.sql
@@ -0,0 +1,8 @@
+/*
+problem link:  https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+status :accepted
+problem name:Average Population
+*/
+
+
+select name from CITY  where population > 120000 and CountryCode='USA' ; 
