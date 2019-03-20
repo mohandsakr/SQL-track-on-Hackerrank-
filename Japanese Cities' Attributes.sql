6225707b7b66ea490ae956906db8812f0ef394d3
mohand reda sakr <mohandsakr69@gmail.com>
2019-03-20 14:56:17 +0200

    Create Japanese Cities' Attributes.sql

---
+++ Japanese Cities' Attributes.sql
@@ -0,0 +1,6 @@
+/*
+problem link:  https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+status :accepted
+problem name: Japanese Cities' Attributes
+*/
+select * from  CITY where COUNTRYCODE ='JPN';
