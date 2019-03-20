c8569ce685e258266b4a198032ed13a10ee207e7
mohand reda sakr <mohandsakr69@gmail.com>
2019-03-20 15:29:06 +0200

    Update Weather Observation Station 15.sql

--- Weather Observation Station 15.sql
+++ Weather Observation Station 15.sql
@@ -5,7 +5,20 @@ status :accepted
 problem name: Weather Observation Station 15
 */
 
+/* 
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. Round your answer to 4
 
+decimal places.
+
+Input Format
+
+The STATION table is described as follows:
+
+ 
+
+where LAT_N is the northern latitude and LONG_W is the western longitude. 
+
+*/
 
 
 
